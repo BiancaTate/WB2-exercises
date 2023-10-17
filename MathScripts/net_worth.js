39acb0a02acc2c4509e0d1c28be91730e46d483e
BiancaTate <tatebianca1204@gmail.com>
2023-10-17 14:59:39 -0400

    Net Worth calculation

---
+++ MathScripts/net_worth.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let assets = 1000
+let debt = 200
+let netWorth = assets - debt
+
+console.log("Your net worth is " + netWorth)
---
+++ intro.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let number = -9.1
+//..
+numbers = 67
+
+console.log(number)
+console.log(numbers)

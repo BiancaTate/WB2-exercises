907318d0f8bc77e830f781fe801cc90108eb32ad
BiancaTate <tatebianca1204@gmail.com>
2023-10-17 15:15:22 -0400

    tip amount exercise

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// food bill + tip percentage/100 = tip
+
+let foodBill = 15.00
+let tipPercent = 12/100
+let tip = foodBill * tipPercent
+
+console.log("The tip on a $" + foodBill + " food bill is $" + tip.toFixed(2))

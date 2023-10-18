081ccaed1f20e5294fd79403f7dc213dd16003b4
BiancaTate <tatebianca1204@gmail.com>
2023-10-18 15:32:50 -0400

    "parseInt conversion"

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,19 @@
+"use strict"
+
+// Description: This script tests various numeric // conversion techniques
+// Author: Bianca Tate 
+
+let a=" 101.1 "
+let b = "55"
+let c = "402 Stevens"
+let d = "Number 5 "
+
+a=parseInt(a)
+b=parseInt(b)
+c=parseInt(c)
+d=parseInt(d)
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)

e74f1f4f09227d90ca8fe76a9500ef14c1fc6545
BiancaTate <tatebianca1204@gmail.com>
2023-10-18 09:32:08 -0400

    "changed tip from fraction to a decimal"

--- MathScripts/tip_amount.js
+++ MathScripts/tip_amount.js
@@ -3,7 +3,7 @@
 // food bill + tip percentage/100 = tip
 
 let foodBill = 15.00
-let tipPercent = 12/100
+let tipPercent = 0.12
 let tip = foodBill * tipPercent
 
 console.log("The tip on a $" + foodBill + " food bill is $" + tip.toFixed(2))
